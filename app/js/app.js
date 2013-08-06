1da931551b3ee59539ee3b1da2a42d7e819bd5bc
Dominik Weiss <dominik@railsland.com>
2013-08-06 22:04:35 +0200

    using resolve

--- app/js/app.js
+++ app/js/app.js
@@ -10,42 +10,52 @@ app.config(["$routeProvider", function($routeProvider) {
     // by using resolve the controller gets loaded when the xhr request finished
     resolve: {
       'recipesData': function(RecipesService) {
-        return RecipesService.promise;
+        return RecipesService.getIndexPromise();
       }
     }
   })
   .when("/details/:id", {
     templateUrl: "views/details.html",
-    controller: "DetailsCtrl"
+    controller: "DetailsCtrl",
+    resolve: {
+      'recipeData': function($route, RecipesService) {
+        return RecipesService.getOnePromise($route.current.params.id);
+      }
+    }
   });
 }]);
 
 // get all the recipes
 app.service("RecipesService", function($http) {
-  var recipes = new Array;
-
   // a service in Angular is a singleton, so this is only executed once
-  var promise = $http.get('js/recipes.json').then(function(response) {
-    recipes = response.data;
-  }, function(reason) {
-    alert("Failed fetching recipes (Status " + reason.status + ")");
-  });
+  var getIndexPromise = function() {
+    console.log("getting the data now, all of it")
+    return $http.get('js/recipes.json').then(function(response) {
+      return response.data;
+    }, function(reason) {
+      alert("Failed fetching recipes (Status " + reason.status + ")");
+    });
+  }
+
+  var getOnePromise = function(id) {
+    console.log("getting the data now, one only")
+    return $http.get('js/recipes.json').then(function(response) {
+      return _.find(response.data, function(r) { return r.id == id });
+    }, function(reason) {
+      alert("Failed fetching recipe (Status " + reason.status + ")");
+    });
+  }
 
   return {
-    promise: promise,
-    getAll: function() {
-      return recipes;
-    },
-    getOne: function (id) {
-      return _.find(recipes, function(r) { return r.id == id });
-    }
+    getIndexPromise: getIndexPromise,
+    getOnePromise: getOnePromise
   };
 });
 
-app.controller('AllRecipesCtrl', function($scope, $timeout, RecipesService) {
-  $scope.recipes = RecipesService.getAll();
+app.controller('AllRecipesCtrl', function($scope, recipesData) {
+  $scope.recipes = recipesData;
 });
 
-app.controller('DetailsCtrl', function($scope, $routeParams, RecipesService) {
-  $scope.recipe = RecipesService.getOne($routeParams.id);
+app.controller('DetailsCtrl', function($scope, recipeData) {
+  $scope.recipe = recipeData;
 });
