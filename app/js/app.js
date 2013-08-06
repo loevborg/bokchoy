8e733ed53ee6f4dabdec17a311dd514e1ee2e62f
Dominik Weiss <dominik@railsland.com>
2013-08-06 22:17:38 +0200

    cleanup

--- app/js/app.js
+++ app/js/app.js
@@ -7,10 +7,10 @@ app.config(["$routeProvider", function($routeProvider) {
   .when("/", {
     templateUrl: "views/all.html",
     controller: "AllRecipesCtrl",
-    // by using resolve the controller gets loaded when the xhr request finished
     resolve: {
+      // recipesData is now available in the controller
       'recipesData': function(RecipesService) {
-        return RecipesService.getIndexPromise();
+        return RecipesService.getIndex();
       }
     }
   })
@@ -18,8 +18,10 @@ app.config(["$routeProvider", function($routeProvider) {
     templateUrl: "views/details.html",
     controller: "DetailsCtrl",
     resolve: {
+      // $routeParams does not work because it is only available after
+      // the route has changed
       'recipeData': function($route, RecipesService) {
-        return RecipesService.getOnePromise($route.current.params.id);
+        return RecipesService.getOne($route.current.params.id);
       }
     }
   });
@@ -27,31 +29,29 @@ app.config(["$routeProvider", function($routeProvider) {
 
 // get all the recipes
 app.service("RecipesService", function($http) {
-  // a service in Angular is a singleton, so this is only executed once
   var getIndexPromise = function() {
-    console.log("getting the data now, all of it")
     return $http.get('js/recipes.json').then(function(response) {
       return response.data;
     }, function(reason) {
       alert("Failed fetching recipes (Status " + reason.status + ")");
     });
-  }
+  };
 
   var getOnePromise = function(id) {
-    console.log("getting the data now, one only")
-    return $http.get('js/recipes.json').then(function(response) {
-      return _.find(response.data, function(r) { return r.id == id });
-    }, function(reason) {
-      alert("Failed fetching recipe (Status " + reason.status + ")");
+    // getIndexPromise is a function that returns a promise
+    // thats why we can call then() here
+    return getIndexPromise().then(function(data) {
+      return _.find(data, function(r) { return r.id == id });
     });
-  }
+  };
 
   return {
-    getIndexPromise: getIndexPromise,
-    getOnePromise: getOnePromise
+    getIndex: getIndexPromise,
+    getOne: getOnePromise
   };
 });
 
+// when the controller is fired, recipeData is already loaded (-> resolve)
 app.controller('AllRecipesCtrl', function($scope, recipesData) {
   $scope.recipes = recipesData;
 });
